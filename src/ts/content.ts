2672575ce9c82cf1f77ece222d34dcd29bc4da39
eliasmurcray <eliasmurcray@gmail.com>
2024-06-12 19:41:13 -0400

    Use MutationObserver API for asynchronous detection of child elements added to the body.

--- src/ts/content.ts
+++ src/ts/content.ts
@@ -1,77 +1,60 @@
+import { requireId } from "../utils/dom-utils";
+
 const split = window.location.pathname.split("/");
+const isComputerScience: boolean = split[1] === "computer-programming" || split[1] === "cs";
+
+function getTabId(qaExpandType: string | null): string {
+	switch (qaExpandType) {
+		case "question":
+		case "answer":
+			return "ka-uid-discussiontabbedpanel-0--tabbedpanel-tab-1";
+		case "comment":
+		case "reply":
+		default:
+			return "ka-uid-discussiontabbedpanel-0--tabbedpanel-tab-2";
+		case "project_help_question":
+			return "ka-uid-discussiontabbedpanel-0--tabbedpanel-tab-0";
+	}
+}
 
 // Project page
-if (
-	(split[1] === "computer-programming" || split[1] === "cs") &&
-	/^\d{16}$/.test(split[3].split("?")[0])
-) {
+if (isComputerScience && /^\d{16}$/.test(split[3].split("?")[0])) {
 	const fetchOverrideScript = document.createElement("script");
 	fetchOverrideScript.src = chrome.runtime.getURL("fetch-override.js");
 	document.body.appendChild(fetchOverrideScript);
 
 	// Set feedback tab based on expand type
 	const qaExpandType = new URLSearchParams(window.location.search).get("qa_expand_type");
-	let iter = 10000; // Retry amount
-	const openFeedbackTab = () => {
-		if (!--iter) return;
-		let button;
-
-		switch (qaExpandType) {
-			case "question":
-			case "answer":
-				button = document.getElementById(
-					"ka-uid-discussiontabbedpanel-0--tabbedpanel-tab-1",
-				) as HTMLButtonElement;
-				break;
-			case "comment":
-			case "reply":
-			case null:
-				button = document.getElementById(
-					"ka-uid-discussiontabbedpanel-0--tabbedpanel-tab-2",
-				) as HTMLButtonElement;
-				break;
-			case "project_help_question":
-				button = document.getElementById(
-					"ka-uid-discussiontabbedpanel-0--tabbedpanel-tab-0",
-				) as HTMLButtonElement;
-				break;
-		}
-
-		if (!button || !(button instanceof HTMLButtonElement)) {
-			window.requestAnimationFrame(openFeedbackTab);
-			return;
-		}
-
-		button.click();
+	requireId(getTabId(qaExpandType)).then((qaTab) => {
+		if (!(qaTab instanceof HTMLButtonElement)) return;
+		qaTab.click();
 		window.scrollTo(0, 0);
 
-		// Auto sort comments
-		chrome.storage.local.get("defaultCommentSort", ({ defaultCommentSort }) => {
+		// Sort comments
+		chrome.storage.local.get("defaultCommentSort", async ({ defaultCommentSort }) => {
 			if (!defaultCommentSort) return;
-			iter = 10000; // Retry amount
-			const setCommentSort = () => {
-				if (!--iter) return;
-				const button = document.getElementById("sortBy");
-				if (!button || !(button instanceof HTMLButtonElement)) {
-					window.requestAnimationFrame(setCommentSort);
-					return;
+			const sortDropdownButton = await requireId("sortBy");
+			if (!(sortDropdownButton instanceof HTMLButtonElement)) return;
+			sortDropdownButton.click();
+			const sortButtons = document.querySelectorAll<HTMLButtonElement>(
+				"div[data-test-id='dropdown-core-container'] button",
+			);
+			sortButtons.forEach((sortButton) => {
+				if (sortButton.innerText.includes(defaultCommentSort)) {
+					sortButton.click();
 				}
-				button.click();
-
-				const sortButtons = document.querySelectorAll<HTMLButtonElement>(
-					"div[data-test-id='dropdown-core-container'] button",
-				);
-				sortButtons.forEach((btn) => {
-					if (btn.innerHTML.includes(defaultCommentSort)) {
-						btn.click();
-					}
-				});
-				window.scrollTo(0, 0);
-				button.blur();
-			};
-			setCommentSort();
+			});
+			window.scrollTo(0, 0);
+			if (document.activeElement && document.activeElement instanceof HTMLElement) {
+				document.activeElement.blur();
+			}
 		});
-	};
+	});
+}
 
-	window.requestAnimationFrame(openFeedbackTab);
+// New program
+if (isComputerScience && split[2] === "new") {
+	const aceOverrideScript = document.createElement("script");
+	aceOverrideScript.src = chrome.runtime.getURL("ace-override.js");
+	document.body.appendChild(aceOverrideScript);
 }
